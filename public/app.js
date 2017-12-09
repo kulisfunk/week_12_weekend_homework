07b3ed17e2d1eb06aaa062904233a60c0cfc7730
kulisfunk <kulisfunk@hotmail.com>
2017-12-09 15:24:18 +0000

    added xmlhttp request to app

--- public/app.js
+++ public/app.js
@@ -1,8 +1,19 @@
 var app = function(){
   console.log('app running');
+var url = "https://api.nasa.gov/planetary/apod?api_key=vtn8biNvmVkMX5wFK7NVaptFQTDijsOEI18PBdLx"
+ makeRequest(url, requestComplete);
 
 
 }
+
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+}
+
+
 
 
 window.addEventListener('load', app);
