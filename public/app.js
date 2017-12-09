dec627b14a521d498a40f8267639d1cdb0d26d8e
kulisfunk <kulisfunk@hotmail.com>
2017-12-09 19:43:18 +0000

    added dynamic search bar to contruct url for request

--- public/app.js
+++ public/app.js
@@ -1,16 +1,34 @@
 var app = function(){
   console.log('app running');
-// var url = "https://api.nasa.gov/planetary/apod?api_key=vtn8biNvmVkMX5wFK7NVaptFQTDijsOEI18PBdLx"
-// var url = "https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image"
 
-var url = "https://images-api.nasa.gov/search?q=apollo%2010&description=moon&media_type=image"
+var url = "https://images-api.nasa.gov/search?q=jupiter&description=&media_type=image"
 
 
 
  makeRequest(url, requestComplete);
 
+ var searchButton = document.getElementById('search-button');
+
+ searchButton.addEventListener('click', function() {
+ 			search();
+ 		});
+ 	}
 
 
+
+var search = function(){
+  var search1 = document.getElementById('search1').value;
+  var search2 = document.getElementById('search2').value;
+  var search3 = document.getElementById('search3').value;
+  if (search3 === ''){
+    var url = "https://images-api.nasa.gov/search?q=" + search1.value + "&description=" + search2.value;
+  }else{
+  var url = "https://images-api.nasa.gov/search?q=" + search1.value + "&description=" + search2.value + "&media_type=" + search3.value
+}
+  makeRequest(url, requestComplete);
+  document.getElementById('search1').value = '';
+  document.getElementById('search2').value = '';
+  document.getElementById('search3').value = '';
 }
 
 var makeRequest = function(url, callback){
@@ -26,8 +44,10 @@ var requestComplete = function(){
   }
   var jsonString = this.responseText;
   var objectList = JSON.parse(jsonString);
-  console.log(objectList);
   var items = objectList.collection.items;
+  if(items.length ===  0){
+    return;
+  }
   console.log(items);
   populateSelect(items);
 
@@ -35,6 +55,7 @@ var requestComplete = function(){
 
 var populateSelect = function(items){
   var select = document.getElementById('picture-select');
+  select.options.length = 0;
   items.forEach(function(item){
     var option = document.createElement('option');
     option.innerText = item.data[0].title;
@@ -57,20 +78,13 @@ var selectChanged = function(index, items){
  link.href = item.links[0].href;
  link.innerText = item.data[0].title;
  description.innerText = item.data[0].description;
-//  var ingredients = recipe.ingredients.split(', ');
-//  ingredients.forEach(function(ingredient){
-//    var item = document.createElement('li');
-//    item.innerText = ingredient;
-//    list.appendChild(item);
-//  });
  image.src = item.links[0].href;
  image.alt = item.data[0].title;
 }
 
 
 
+
+
+
 window.addEventListener('load', app);
-
-
-// collection:
-// items: 0-99
