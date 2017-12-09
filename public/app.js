dab0cf23c366a27e1562acdced9c866cd28fe2f2
kulisfunk <kulisfunk@hotmail.com>
2017-12-09 16:26:21 +0000

    populated drop down with photo titles

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 var app = function(){
   console.log('app running');
 // var url = "https://api.nasa.gov/planetary/apod?api_key=vtn8biNvmVkMX5wFK7NVaptFQTDijsOEI18PBdLx"
-var url = "https://images-api.nasa.gov/search?q=apollo%2011"
+var url = "https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image"
  makeRequest(url, requestComplete);
+
 
 
 }
@@ -21,13 +22,29 @@ var requestComplete = function(){
   var jsonString = this.responseText;
   var objectList = JSON.parse(jsonString);
   console.log(objectList);
+  var items = objectList.collection.items;
+  console.log(items);
+  populateSelect(items);
 
 }
+
+var populateSelect = function(items){
+  var select = document.getElementById('picture-select');
+  items.forEach(function(item){
+    var option = document.createElement('option');
+    option.innerText = item.data[0].title;
+    select.appendChild(option);
+  });
+}
+  // selectChanged(select.selectedIndex, recipes);//shows index 0 in list on page.......
+  // select.addEventListener('change', function(){
+  //   selectChanged(select.selectedIndex, recipes);
+  // }.bind(this));
 
 
 
 window.addEventListener('load', app);
 
 
-collection:
-items: 0-99
+// collection:
+// items: 0-99
